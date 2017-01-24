5578f7fa36f938dfc3a4309b46da2da0b396bc9f
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2017-01-25 00:44:28 +0300

    docs: add stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp           = require('gulp'),
     sourcemaps     = require('gulp-sourcemaps'),
     gulpIf         = require('gulp-if'),
     watch          = require('gulp-watch'),
+    stylelint      = require('gulp-stylelint'),
     browserSync    = require('browser-sync').create();
 
 
@@ -12,14 +13,22 @@ var isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'developme
 
 gulp.task('serve', function() {
   browserSync.init({
-    server: './build'
-  })
+    server: {
+      baseDir: "build"
+    }
+  });
 });
 
 
 gulp.task('styles', function() {
   return gulp.src('app/assets/styles/main.scss')
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(stylelint({
+      reporters: [{
+        formatter: 'string',
+        console: true
+      }]
+    }))
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(gulpIf(isDevelopment, sourcemaps.write()))
